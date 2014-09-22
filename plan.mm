8246c0577f6231d005a8f52614328eff7cab6269
Sebastien Campion <sebastien.campion@inria.fr>
2014-09-22 22:26:29 +0200

    Pratiques open source

--- plan.mm
+++ plan.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.2.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Open Source &amp; Recherche" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1411392676242"><hook NAME="MapStyle">
+<node TEXT="Open Source &amp; Recherche" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1413400956428"><hook NAME="MapStyle">
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node">
@@ -49,28 +49,70 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
-<node TEXT="Intro" POSITION="right" ID="ID_469446735" CREATED="1411392800851" MODIFIED="1411392805356">
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+<node TEXT="Intro" POSITION="right" ID="ID_469446735" CREATED="1411392800851" MODIFIED="1413400956433">
 <edge COLOR="#ff0000"/>
-<node TEXT="Le d&#xe9;velloppement dans la recherche pose la question de la publication" ID="ID_705998756" CREATED="1411392805501" MODIFIED="1411392827257"/>
-<node TEXT="L&apos;industrie aussi a adopt&#xe9; l&apos;open source mais pour diff&#xe9;rente raison" ID="ID_1976758164" CREATED="1411392827621" MODIFIED="1411392852361"/>
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+<node TEXT="Le d&#xe9;velloppement dans la recherche pose la question de la publication" ID="ID_705998756" CREATED="1411392805501" MODIFIED="1413400956434">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
 </node>
-<node TEXT="Droit(s)" POSITION="left" ID="ID_1208617239" CREATED="1411392860357" MODIFIED="1411392869161">
+<node TEXT="L&apos;industrie aussi a adopt&#xe9; l&apos;open source mais pour diff&#xe9;rente raison" ID="ID_1976758164" CREATED="1411392827621" MODIFIED="1413400956436">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Droit(s)" POSITION="left" ID="ID_1208617239" CREATED="1411392860357" MODIFIED="1413400956437">
 <edge COLOR="#0000ff"/>
-<node TEXT="Le droit d&apos;auteur et patrimonial" ID="ID_781133827" CREATED="1411395680208" MODIFIED="1411395723469"/>
-<node TEXT="Partager quoi ?" ID="ID_42920544" CREATED="1411395702360" MODIFIED="1411395753090"/>
-<node TEXT="Pieges" ID="ID_233970655" CREATED="1411395804853" MODIFIED="1411395806961"/>
-<node TEXT="Open Source / Logiciel libre" ID="ID_477519625" CREATED="1411395777395" MODIFIED="1411395784966"/>
-<node TEXT="Pourquoi partager" ID="ID_1539628561" CREATED="1411396359628" MODIFIED="1411396364841">
-<node TEXT="Motivations" ID="ID_109921315" CREATED="1411396404807" MODIFIED="1411396412092"/>
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+<node TEXT="Le droit d&apos;auteur et patrimonial" ID="ID_781133827" CREATED="1411395680208" MODIFIED="1413400956440">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
 </node>
-<node TEXT="Quelles licences ?" ID="ID_1263090833" CREATED="1411412325099" MODIFIED="1411412332863"/>
+<node TEXT="Partager quoi ?" ID="ID_42920544" CREATED="1411395702360" MODIFIED="1413400956442">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
 </node>
-<node TEXT="Diffusion et cons&#xe9;quences" POSITION="left" ID="ID_1180849490" CREATED="1411414350723" MODIFIED="1411414358799">
+<node TEXT="Pieges" ID="ID_233970655" CREATED="1411395804853" MODIFIED="1413400956443">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+<node TEXT="Open Source / Logiciel libre" ID="ID_477519625" CREATED="1411395777395" MODIFIED="1413400956445">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+<node TEXT="Pourquoi partager" ID="ID_1539628561" CREATED="1411396359628" MODIFIED="1413400956448">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+<node TEXT="Motivations" ID="ID_109921315" CREATED="1411396404807" MODIFIED="1413400956449">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Quelles licences ?" ID="ID_1263090833" CREATED="1411412325099" MODIFIED="1413400956450">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Diffusion et cons&#xe9;quences" POSITION="left" ID="ID_1180849490" CREATED="1411414350723" MODIFIED="1413400956451">
 <edge COLOR="#00ff00"/>
-<node TEXT="Les utilisateurs" ID="ID_56005260" CREATED="1411414359323" MODIFIED="1411414362175"/>
-<node TEXT="Les contributeurs" ID="ID_1425830882" CREATED="1411415781109" MODIFIED="1411415785360"/>
-<node TEXT="Les d&#xe9;veloppeurs" ID="ID_258521696" CREATED="1411416736720" MODIFIED="1411416743027"/>
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+<node TEXT="Les utilisateurs" ID="ID_56005260" CREATED="1411414359323" MODIFIED="1413400956452">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+<node TEXT="Les contributeurs" ID="ID_1425830882" CREATED="1411415781109" MODIFIED="1413400956453">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+<node TEXT="Les d&#xe9;veloppeurs" ID="ID_258521696" CREATED="1411416736720" MODIFIED="1413400956454">
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Guide de publication Open Source" POSITION="right" ID="ID_422324529" CREATED="1411414350723" MODIFIED="1413401209162">
+<edge COLOR="#00ffff"/>
+<font NAME="Inconsolata" SIZE="16" BOLD="false"/>
+<node TEXT="Bien d&#xe9;marrer" ID="ID_496612177" CREATED="1413401226928" MODIFIED="1413402117050">
+<node TEXT="Commencer avec ce que vous avez" ID="ID_1584591737" CREATED="1413402118194" MODIFIED="1413402131223"/>
+<node TEXT="Choisir une licence et l&apos;appliquer" ID="ID_861514071" CREATED="1413402132155" MODIFIED="1413402145398"/>
+<node TEXT="Donner le ton" ID="ID_328860785" CREATED="1413402147907" MODIFIED="1413402150158"/>
+<node TEXT="Annoncer" ID="ID_335610100" CREATED="1413402152011" MODIFIED="1413402153807"/>
+</node>
+<node TEXT="Infra Technique" ID="ID_129463576" CREATED="1413402168860" MODIFIED="1413402182592"/>
+<node TEXT="Infra Sociale" ID="ID_726966887" CREATED="1413402212516" MODIFIED="1413402218815"/>
+<node TEXT="Communication" ID="ID_151788182" CREATED="1413402229714" MODIFIED="1413402232656"/>
+<node TEXT="Paquets, sortie et d&#xe9;v au quotidien" ID="ID_800620944" CREATED="1413402232947" MODIFIED="1413402251904"/>
+<node TEXT="Encadrer les volontaires" ID="ID_1482740281" CREATED="1413402257131" MODIFIED="1413402264176"/>
 </node>
 </node>
 </map>
