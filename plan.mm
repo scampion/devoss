c55768f1144defe00db47e35b252b619d9997098
Sebastien Campion <sebastien.campion@inria.fr>
2014-09-22 22:26:14 +0200

    complete transcript, todo include whats a soft is ? from cvstene slides

---
+++ plan.mm
@@ -0,0 +1,76 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Open Source &amp; Recherche" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1411392676242"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="Intro" POSITION="right" ID="ID_469446735" CREATED="1411392800851" MODIFIED="1411392805356">
+<edge COLOR="#ff0000"/>
+<node TEXT="Le d&#xe9;velloppement dans la recherche pose la question de la publication" ID="ID_705998756" CREATED="1411392805501" MODIFIED="1411392827257"/>
+<node TEXT="L&apos;industrie aussi a adopt&#xe9; l&apos;open source mais pour diff&#xe9;rente raison" ID="ID_1976758164" CREATED="1411392827621" MODIFIED="1411392852361"/>
+</node>
+<node TEXT="Droit(s)" POSITION="left" ID="ID_1208617239" CREATED="1411392860357" MODIFIED="1411392869161">
+<edge COLOR="#0000ff"/>
+<node TEXT="Le droit d&apos;auteur et patrimonial" ID="ID_781133827" CREATED="1411395680208" MODIFIED="1411395723469"/>
+<node TEXT="Partager quoi ?" ID="ID_42920544" CREATED="1411395702360" MODIFIED="1411395753090"/>
+<node TEXT="Pieges" ID="ID_233970655" CREATED="1411395804853" MODIFIED="1411395806961"/>
+<node TEXT="Open Source / Logiciel libre" ID="ID_477519625" CREATED="1411395777395" MODIFIED="1411395784966"/>
+<node TEXT="Pourquoi partager" ID="ID_1539628561" CREATED="1411396359628" MODIFIED="1411396364841">
+<node TEXT="Motivations" ID="ID_109921315" CREATED="1411396404807" MODIFIED="1411396412092"/>
+</node>
+<node TEXT="Quelles licences ?" ID="ID_1263090833" CREATED="1411412325099" MODIFIED="1411412332863"/>
+</node>
+<node TEXT="Diffusion et cons&#xe9;quences" POSITION="left" ID="ID_1180849490" CREATED="1411414350723" MODIFIED="1411414358799">
+<edge COLOR="#00ff00"/>
+<node TEXT="Les utilisateurs" ID="ID_56005260" CREATED="1411414359323" MODIFIED="1411414362175"/>
+<node TEXT="Les contributeurs" ID="ID_1425830882" CREATED="1411415781109" MODIFIED="1411415785360"/>
+<node TEXT="Les d&#xe9;veloppeurs" ID="ID_258521696" CREATED="1411416736720" MODIFIED="1411416743027"/>
+</node>
+</node>
+</map>
